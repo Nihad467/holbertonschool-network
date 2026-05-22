10f5e3bf5213781da3b6b841ce34a7d3066af4aa
Nihad467 <nihadseferzade01@gmail.com>
2026-05-23 00:38:16 +0400

    0. What's my status?

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+﻿#!/usr/bin/python3
+"""Fetches a URL and displays response information using urllib."""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5050/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
